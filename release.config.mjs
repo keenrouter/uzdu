7d5c3ebdac944d648a164826bdba52ebb7b6857b
Denis Kalinin <dev@keenrouter.net>
2025-12-04 23:44:48 +0300

    ci: remove semantic-release/npm

--- release.config.mjs
+++ release.config.mjs
@@ -1,11 +1,11 @@
 /**
  * @type {{ plugins: (string | [string, {pkgRoot: string}])[]}}
+ *     ["@semantic-release/npm", {pkgRoot: "distTemp"}],
  */
 export default {
   branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    ["@semantic-release/npm", {pkgRoot: "distTemp"}],
+    "@semantic-release/release-notes-generator"
   ]
 };
